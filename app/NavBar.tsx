d61694f7bd9126519715e5bbfe717156e53ad108
htack210 <htack210@live.com>
2024-07-02 16:46:53 -0400

    Change navbar layout

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,7 +6,7 @@ import React from "react";
 import { useSession } from "next-auth/react";
 import { FaBug } from "react-icons/fa";
 import classnames from "classnames";
-import { Box } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
@@ -23,36 +23,42 @@ const NavBar = () => {
     },
   ];
   return (
-    <nav className="flex space-x-6 border-b border-t mb-5 px-5 h-14 items-center">
-      <Link href="/">
-        <FaBug />
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              className={classnames({
-                "border-2 rounded-tl-none rounded-br-none border-sky-600":
-                  link.href === currentPath,
-                "border-0": link.href !== currentPath,
-                "text-sky-500 hover:bg-sky-500 hover:text-sky-100 rounded-xl px-4 transition-colors font-semibold":
-                  true,
-              })}
-              href={link.href}
-            >
-              {link.label}
+    <nav className=" border-b border-t mb-5 px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <FaBug />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "authenticated" && (
-          <Link href="/api/auth/signout">Logout</Link>
-        )}
-        {status === "unauthenticated" && (
-          <Link href="/api/auth/signin">Login</Link>
-        )}
-      </Box>
+            <ul className="flex space-x-6">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    className={classnames({
+                      "border-2 rounded-tl-none rounded-br-none border-sky-600":
+                        link.href === currentPath,
+                      "border-0": link.href !== currentPath,
+                      "text-sky-500 hover:bg-sky-500 hover:text-sky-100 rounded-xl px-4 transition-colors font-semibold":
+                        true,
+                    })}
+                    href={link.href}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && (
+              <Link href="/api/auth/signout">Logout</Link>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Login</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
