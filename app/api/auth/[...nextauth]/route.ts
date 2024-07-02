dc890e7eb7a368c6c2f85432cf7cd1caf7642fd0
htack210 <htack210@live.com>
2024-07-02 14:58:24 -0400

    Secrets OKd

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,10 @@ import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
     providers: [
-
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        })
     ]
 })
 
