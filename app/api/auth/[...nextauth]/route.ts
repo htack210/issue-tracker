099782417195ac260ef0e0b09eef95a9ffc53020
htack210 <htack210@live.com>
2024-07-02 14:54:27 -0400

    Removing secrets

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,10 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
     providers: [
-        //     GoogleProvider({
-        //     clientId: process.env.GOOGLE_CLIENT_ID!,
-        //     clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-        // })
+
     ]
 })
 
