97d05bd7fe591e19bdb0072041752c37f00b9ef8
htack210 <htack210@live.com>
2024-07-04 19:19:15 -0400

    Fix filtering bugs

--- app/issues/list/issueStatusFilter.tsx
+++ app/issues/list/issueStatusFilter.tsx
@@ -2,7 +2,7 @@
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const statuses: { label: string; value?: Status }[] = [
@@ -14,11 +14,19 @@ const statuses: { label: string; value?: Status }[] = [
 
 const issueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") || ""}
       onValueChange={(status) => {
-        const query = status === "All" ? "" : `?status=${status}`;
+        const params = new URLSearchParams();
+        status === "All" ? "" : `?status=${status}`;
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+
+        const query = params.size ? "?" + params.toString() : "";
         router.push(`/issues/list${query}`);
       }}
     >
